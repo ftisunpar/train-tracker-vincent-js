3a113c0355531bdc14f3149478be329ec1fa3fd9
iyosskoyos <iyoss.koyos@yahoo.com>
2018-02-07 08:26:00 +0700

    benerin set notif

---
+++ app/src/main/java/com/example/hengky/proiftraintracker/SetNotif.java
@@ -0,0 +1,19 @@
+package com.example.hengky.proiftraintracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Spinner;
+
+/**
+ * Created by Lenovo Iyoss on 07/02/2018.
+ */
+
+public class SetNotif extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+
+    }
+}
