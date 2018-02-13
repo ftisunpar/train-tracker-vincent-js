ec288c8af94722ecddba247efd0bb34079028bf1
Hengky Surya <hengkihan@yahoo.com>
2018-02-13 13:56:51 +0700

    Kemajuan Sedikit

--- app/src/main/java/com/example/hengky/proiftraintracker/OnProgress.java
+++ app/src/main/java/com/example/hengky/proiftraintracker/OnProgress.java
@@ -1,8 +1,17 @@
 package com.example.hengky.proiftraintracker;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Hengky on 2/10/2018.
  */
 
-public class on_progress {
+public class OnProgress extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.on_progress_trip);
+
+    }
 }
