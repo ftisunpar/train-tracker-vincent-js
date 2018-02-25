a9d3e54fe073e9366a2a8ab644405e06cdc177e0
vincentjs20 <vincentyoelsinatra2@gmail.com>
2018-02-25 16:13:07 +0700

    request location permission, speedometer modification
    - now the app will ask the permission to access location
    - the app can meassure the speed when the user is moving

--- app/src/main/java/com/example/hengky/proiftraintracker/ChooseDestination.java
+++ app/src/main/java/com/example/hengky/proiftraintracker/ChooseDestination.java
@@ -1,13 +1,20 @@
 package com.example.hengky.proiftraintracker;
 
+import android.*;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
-
+import android.Manifest;
+import android.widget.Toast;
 
 
 import java.util.Arrays;
@@ -16,7 +23,8 @@ import java.util.List;
 
 
 public class ChooseDestination extends AppCompatActivity {
-
+    private static final int MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 10;
+    Button buttonMap, buttonGo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +44,69 @@ public class ChooseDestination extends AppCompatActivity {
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, list_stasiun);
         spinner2.setAdapter(adapter2);
+        reqPermission();
+
+        buttonMap = (Button) findViewById(R.id.btnOpenMap);
+        buttonGo = (Button) findViewById(R.id.btn_go);
+
+        buttonMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMap(view);
+
+            }
+        });
+        buttonGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToTripPage(view);
+            }
+        });
+    }
+
+    private void reqPermission(){
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION
+            );
+        }
+        else{
+            accessLocation();
+        }
+    }
+
+    private void accessLocation() {
+        Toast.makeText(this, "Allow Apps to Access Location", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSION_REQUEST_ACCESS_FINE_LOCATION :
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    accessLocation();
+                }
+                else{
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)){
+                        new AlertDialog.Builder(this).
+                                setTitle("Access Location permission").
+                                setMessage("You need to grant access location permission" +
+                                        "to use this feature. Please retry and grant the permission.").show();
+                    }
+                    else {
+                        new AlertDialog.Builder(this).
+                                setTitle("Access Location permission dennied").setMessage("You dennied" +
+                                "access location permission. So, the feature will be disabled." +
+                                "To enable it, go on setting -> app -> Train Tracker and grant location service permission").show();
+                    }
+                }
+
+                break;
+        }
     }
 
     public void moveToTripPage(View view){
