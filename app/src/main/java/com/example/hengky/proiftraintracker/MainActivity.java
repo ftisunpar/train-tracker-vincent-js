16cff493c7a318877c032e40cd49160a0c9b91c8
Unknown <astefanus44@gmail.com>
2018-03-01 08:57:17 +0700

    Connect to Firebase
    works

--- app/src/main/java/com/example/hengky/proiftraintracker/MainActivity.java
+++ app/src/main/java/com/example/hengky/proiftraintracker/MainActivity.java
@@ -2,23 +2,20 @@ package com.example.hengky.proiftraintracker;
 
 import android.os.Bundle;
 import android.content.Intent;
-import android.service.chooser.ChooserTargetService;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Filter;
-import android.widget.Toast;
 
-import com.ajithvgiri.searchdialog.*;
-import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import ir.mirrajabi.searchdialog.SimpleSearchDialogCompat;
-import ir.mirrajabi.searchdialog.SimpleSearchFilter;
 import ir.mirrajabi.searchdialog.core.BaseSearchDialogCompat;
 import ir.mirrajabi.searchdialog.core.SearchResultListener;
 import ir.mirrajabi.searchdialog.core.Searchable;
@@ -29,17 +26,33 @@ import ir.mirrajabi.searchdialog.core.Searchable;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-    private DatabaseReference rootRef;
+    DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference("listKereta");
+
+    ArrayList<String> List = new ArrayList<>();
     private Button sendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
-        if(!FirebaseApp.getApps(this).isEmpty()) {
-            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-        }
-        rootRef = FirebaseDatabase.getInstance().getReference();
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String name = ds.getValue(String.class);
+                    List.add(name);
+                    Log.d("-------------------", name);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                List.add("gagal");
+            }
+        };
+        rootRef.addValueEventListener(eventListener);
+
+
+
         this.sendData = this.findViewById(R.id.tes);
         this.sendData.setOnClickListener(this);
 
@@ -68,8 +81,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private ArrayList<SearchModel> getData(){
         ArrayList<SearchModel> list = new ArrayList<>();
-        String[] some_array = getResources().getStringArray(R.array.list_kereta);
-        for (int i = 0; i < getResources().getStringArray(R.array.list_kereta).length; i++){
+        String[] some_array = new String[List.size()];
+        for (int i=0 ; i<List.size();i++){
+            some_array[i]=List.get(i);
+        }
+        for (int i = 0; i < some_array.length; i++){
             SearchModel model = new SearchModel(some_array[i]);
             list.add(model);
         }
@@ -78,14 +94,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return list;
     }
 
-    private ArrayList<SampleSearchModel> createSampleData(){
-        ArrayList<SampleSearchModel> items = new ArrayList<>();
-        String[] some_array = getResources().getStringArray(R.array.list_kereta);
-        for (int i = 0; i < getResources().getStringArray(R.array.list_kereta).length; i++){
-            SampleSearchModel searchModel = new SampleSearchModel(some_array[i]);
-            items.add(searchModel);
+    private ArrayList<SearchModel> createSampleData(){
+        ArrayList<SearchModel> list = new ArrayList<>();
+        String[] some_array = new String[List.size()];
+        for (int i=0 ; i<List.size();i++){
+            some_array[i]=List.get(i);
         }
-        return items;
+        for (int i = 0; i < some_array.length; i++){
+            SearchModel model = new SearchModel(some_array[i]);
+            list.add(model);
+        }
+
+
+        return list;
     }
     public void moveToAnotherActivity(String train){
         Intent intent = new Intent(this, ChooseDestination.class);
